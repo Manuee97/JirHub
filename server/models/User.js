f876c70da148b4f5971c01b0f9875ab6b17d0b25
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-12-10 10:15:47 +0100

    Empezando

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = require('mongoose').Schema;
+const IMG_URL = /.*\.(gif|jpe?g|bmp|png)$/igm
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 2 },
+  picture: { type: String, default: 'https://i.stack.imgur.com/l60Hf.png' }
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = doc._id;
+      delete ret._id;
+      delete ret.__v;
+      delete ret.password;
+      delete ret.createdAt;
+      return ret;
+    }
+  }
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
