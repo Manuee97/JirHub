cf1a298d0b6b975499aac6a21d1a128b997fd0de
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-12-18 13:13:38 +0100

    heroku deploy

--- server/app.js
+++ server/app.js
@@ -1,69 +1,76 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
-const session    = require("express-session");
+const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
-const passport = require('passport')
-const cors = require('cors');
+const passport = require("passport");
+const cors = require("cors");
 
+require("./configs/db.config");
+require("./configs/passport.config");
 
-require('./configs/db.config');
-require('./configs/passport.config');
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const whiteList = ['http://localhost:3000','http://localhost:3001',"http://localhost"]
+const whiteList = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost"
+];
 const corsOptions = {
   origin: (origin, cb) => {
     const originIsWhitelisted = whiteList.includes(origin);
-    cb(null, originIsWhitelisted)
+    cb(null, originIsWhitelisted);
   },
   credentials: true
-}
+};
 app.use(cors(corsOptions));
 
-
 // Enable authentication using session + passport
-app.use(session({
-  secret: 'Todo-app',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: false,
-    httpOnly: true,
-    maxAge: 60 * 60 * 24 * 1000
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60
+app.use(
+  session({
+    secret: "Todo-app",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 1000
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60
+    })
   })
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-    
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
-const index = require('./routes');
-app.use('/', index);
+const index = require("./routes");
+app.use("/", index);
 
-app.use((req, res) => {res.sendFile(__dirname + "/public/index.html");});
+app.use((req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 module.exports = app;
