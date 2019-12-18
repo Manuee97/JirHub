93b8ee6288a40703727dc0dad72529e94d9dbe51
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-12-18 13:00:25 +0100

    Otro commit más

---
+++ server/public/precache-manifest.4fb4cffa3be13db1564a230c9c2a53b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c09695c01098d5e9707ec956c35bcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e81ba177723cfbda321e",
+    "url": "/static/css/main.e7b59730.chunk.css"
+  },
+  {
+    "revision": "8010340c9eb878865c09",
+    "url": "/static/js/2.8e8cc97c.chunk.js"
+  },
+  {
+    "revision": "57989e1fc5f47bab013fec8f432ccab6",
+    "url": "/static/js/2.8e8cc97c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e81ba177723cfbda321e",
+    "url": "/static/js/main.14997322.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
