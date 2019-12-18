57c0319b23c419b36809503c5fceaf7ef877ca0a
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-12-18 13:41:47 +0100

    heroku deploy 5

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.DBURL, {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
